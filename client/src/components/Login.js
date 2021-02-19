cd6af5e01dd9e1aed37f7f39087a7f773d637780
sandking-number1 <Alfred.J9412@gmail.com>
2021-02-19 17:16:39 +0000

    Testing URL in constants file

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -19,7 +19,7 @@ export default class Login extends Component {
   }
   onSubmit = (event) => {
     event.preventDefault();
-    fetch(`https://frozen-forest-84312.herokuapp.com/login`, {
+    fetch(`${url}/login`, {
       method: 'POST',
       body: JSON.stringify(this.state),
       headers: {
