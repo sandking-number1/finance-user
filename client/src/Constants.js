6c945822c5530e98d743f0c494536056a26be47d
sandking-number1 <Alfred.J9412@gmail.com>
2021-02-19 16:46:04 +0000

    Change to constants file

--- client/src/Constants.js
+++ client/src/Constants.js
@@ -2,10 +2,10 @@ const prod = {
     url: {
      API_URL: 'frozen-forest-84312.herokuapp.com'
     }};
-    const dev = {
+const dev = {
     url: {
      API_URL: 'http://localhost:5000'
     }
    };
 
-   export const config = process.env.NODE_ENV === 'development' ? dev : prod;
+   export const config = prod;
