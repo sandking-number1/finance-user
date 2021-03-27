3173872118cad0b9710bc63ea11fac1cd491669f
sandking-number1 <Alfred.J9412@gmail.com>
2021-03-27 01:44:50 +0000

    Amending documentation URL

--- client/src/components/Documentation.js
+++ client/src/components/Documentation.js
@@ -1,12 +1,11 @@
 //import './Documentation.css';
 
-export default function Documentation() {
+export default function Documentation (props) {
     return (
       <div className="wrapper">
           <div className="imgdoc">
-              <img src={this.state.business.loan.documents} />
-
-
+              {/*<img src="https://rise-app-40245759.s3-eu-west-1.amazonaws.com/uploads/6051e3f87cfb24fe1d922c2f"/>*/}
+              <img src="https://rise-app-40245759.s3-eu-west-1.amazonaws.com/uploads/6051e3f87cfb24fe1d922c2f"/>
           </div>
 
         </div>
