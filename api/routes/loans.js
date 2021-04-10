8fa2962b7afd3791452545198262c0509fc34247
sandking-number1 <Alfred.J9412@gmail.com>
2021-04-10 22:44:52 +0100

    Commenting out validation of expo push token

--- api/routes/loans.js
+++ api/routes/loans.js
@@ -18,11 +18,12 @@ const notifyUser = ( update, [somePushTokens] ) => {
     // Each push token looks like ExponentPushToken[xxxxxxxxxxxxxxxxxxxxxx]
 
     // Check that all your push tokens appear to be valid Expo push tokens
+   /*
     if (!Expo.isExpoPushToken(pushToken)) {
       console.error(`Push token ${pushToken} is not a valid Expo push token`);
       continue;
     }
-
+*/
     // Construct a message (see https://docs.expo.io/push-notifications/sending-notifications/)
     messages.push({
       to: pushToken,
