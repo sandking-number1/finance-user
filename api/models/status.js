4a7697c810d60708c72655dd9e191862f8b87ecf
sandking-number1 <Alfred.J9412@gmail.com>
2021-05-07 17:16:52 +0100

    Change to timestamp of status field

--- api/models/status.js
+++ api/models/status.js
@@ -4,10 +4,13 @@ const statusSchema = new db.Schema ({
     currentStatus: {
         type: String
     },
+    /*
     createdAt: {
         type: Date,
         default: Date.now
     }
+    */
+   timestamps: true
 });
 
 const Status = db.model('Status', statusSchema);
--- client/src/components/Documentation.js
+++ client/src/components/Documentation.js
@@ -1,11 +1,18 @@
 import Image from 'react-bootstrap/Image';
-import { useParams } from 'react-router-dom';
+import { useParams, useHistory } from 'react-router-dom';
 
 export default function Documentation(props) {
   let { loanId } = useParams();
+  let history = useHistory();
 
   return (
     <div className="wrapper">
+      <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom"></div>
+      <div className="row">
+        <div className ="col">
+        <button class="btn btn-secondary disabled status-badge" onClick={() => history.goBack()}> &lt; BACK</button>
+      </div>
+      </div>
       <Image src={`https://rise-app-40245759.s3-eu-west-1.amazonaws.com/uploads/${loanId}`} fluid />
     </div>
   )
