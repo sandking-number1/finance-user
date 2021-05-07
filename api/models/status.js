32fb8788a24bf3e5022c5ffeb162351c8497e081
sandking-number1 <Alfred.J9412@gmail.com>
2021-05-07 17:31:48 +0100

    Switching status timestamp back to default

--- api/models/status.js
+++ api/models/status.js
@@ -4,13 +4,10 @@ const statusSchema = new db.Schema ({
     currentStatus: {
         type: String
     },
-    /*
     createdAt: {
         type: Date,
         default: Date.now
     }
-    */
-   timestamps: true
 });
 
 const Status = db.model('Status', statusSchema);
